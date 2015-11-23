bde3bb5a4a00c1417afc12177e832b12c56f4940
Andres Vega <gydoar@hotmail.com>
2015-11-23 15:43:27 -0500

    agregando la primera parte del footer

--- footer.php
+++ footer.php
@@ -1,6 +1,33 @@
 
 
+	<footer>
+		<div class="container">
+			<div class="two columns">
+				<ul class="menu__footer">
+					<li><a href="#">INICIO</a></li>
+					<li><a href="#">QUIENES SOMOS</a></li>
+					<li><a href="#">CONTÁCTENOS</a></li>
+				</ul>
+			</div>
 
+			<div class="three columns cubrimiento__footer">
+				<p><img src="images/icon-check.png" alt="">&nbsp;CUBRIMIENTOS EN LONA</p>
+				<div class="row padding-left">
+					<ul class="six columns menu__cubrimiento">
+						<li><a href="#">Kioscos</a></li>
+						<li><a href="#">Membranas</a></li>
+						<li><a href="#">Parasoles</a></li>
+					</ul>
+					<ul class="six columns menu__cubrimiento">
+						<li><a href="#">Pétalos</a></li>
+						<li><a href="#">Sombrillas</a></li>
+						<li><a href="#">Toldos</a></li>
+					</ul>
+				</div>		
+			</div>
+		</div>
+		
+	</footer>
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
