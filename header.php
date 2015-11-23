1c2f464e7baf230c3f64e454d7d7ba61552f9a5f
Andres Vega <gydoar@hotmail.com>
2015-11-23 14:55:59 -0500

    Primer commit terminando el header

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Parasoles y Diseños</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container header">
+    <div class="row">
+      <div class="four columns">
+        <a href="#"><img class="twelve columns" width="318" src="images/logo.png" alt=""></a>
+      </div>
+
+      <div class="seven columns offset-by-one tel__header">
+        <a href="#">(571) 8050573 - 3112238427</a>
+        <a href="#"><img class="btn-nuestros-datos" width="230" src="images/nuestros-datos.png" alt=""></a>
+        <div class="border__gris"></div>
+        <nav class="menu__header">
+          <ul>
+            <li><a href="#">INICIO</a></li>
+            <li><a href="#">QUIENES SOMOS</a></li>
+            <li><a href="#"><img src="images/icon-check.png" alt="">&nbsp;CUBRIMIENTO EN LONA <img src="images/icon-abajo.png" alt=""></a></li>
+            <li><a href="#">CONTÁCTENOS</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <div class="linea__naranja"></div>
