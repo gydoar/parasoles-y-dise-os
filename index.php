a2263cc79fc0ef041d74186478112c3c9a5aa953
Andres Vega <gydoar@hotmail.com>
2015-11-25 13:24:44 -0500

    Cambios en la parte de carpas el home

--- index.php
+++ index.php
@@ -1,36 +1,52 @@
 <?php include "header.php"; ?>
 
-  <div class="container page">
+  <div class="page">
 
-    <div class="row">
-      <div class="three columns">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sapiente voluptatibus modi nulla iste, atque minima perspiciatis, omnis molestias eum, nisi magni pariatur, numquam ex inventore dolorem cumque illum a.
-      </div>
-       <div class="three columns">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sapiente voluptatibus modi nulla iste, atque minima perspiciatis, omnis molestias eum, nisi magni pariatur, numquam ex inventore dolorem cumque illum a.
-      </div>
-       <div class="three columns">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sapiente voluptatibus modi nulla iste, atque minima perspiciatis, omnis molestias eum, nisi magni pariatur, numquam ex inventore dolorem cumque illum a.
-      </div>
-       <div class="three columns">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sapiente voluptatibus modi nulla iste, atque minima perspiciatis, omnis molestias eum, nisi magni pariatur, numquam ex inventore dolorem cumque illum a.
-      </div>
-    </div>
-  
-    <div class="linea__gris"></div>
+    <div class="container">
 
-    <div class="bloque__100">
-       <div class="three columns">
-        <img class="twelve columns" width="255" src="images/calidad100.png" alt="">
+      <div class="row">
+        <div class="three columns product__home">
+          <img class="twelve columns" src="images/kioscos/01.jpg" alt="">
+          <h3>CARPAS NOMBRE</h3>
+          <a class="button" href="#">VER MÁS</a>
+        </div>
+         <div class="three columns product__home">
+          <img class="twelve columns" src="images/kioscos/01.jpg" alt="">
+          <h3>CARPAS NOMBRE</h3>
+          <a class="button" href="#">VER MÁS</a>
+        </div>
+        <div class="three columns product__home">
+          <img class="twelve columns" src="images/kioscos/01.jpg" alt="">
+          <h3>CARPAS NOMBRE</h3>
+          <a class="button" href="#">VER MÁS</a>
+        </div>
+        <div class="three columns">
+          <ul class="boton__home">
+            <li><a href="#">KIOSKOS</a></li>
+            <li><a href="#">MEMBRANAS</a></li>
+            <li><a href="#">PARASOLES</a></li>
+            <li><a href="#">PÉTALOS</a></li>
+            <li><a href="#">SOMBRILLAS</a></li>
+            <li><a href="#">TOLDOS</a></li>
+          </ul>
+        </div>
       </div>
+    
+      <div class="linea__gris"></div>
 
-      <div class="six columns text_100 padding-top">
-        <p>Trabajando con los fabricantes más importantes de la industria textil, sólo escogemos lo mejor para obtener colores sólidosque no destiñen, durabilidad, resistencia del clima y protección solar.</p>
+      <div class="bloque__100">
+         <div class="three columns">
+          <img class="twelve columns" width="255" src="images/calidad100.png" alt="">
+        </div>
+
+        <div class="six columns text_100 padding-top">
+          <p>Trabajando con los fabricantes más importantes de la industria textil, sólo escogemos lo mejor para obtener colores sólidosque no destiñen, durabilidad, resistencia del clima y protección solar.</p>
+        </div>
+
+        <div class="three columns padding-top">
+          <a class="button button-primary" href="#">QUIENES SOMOS</a>
+        </div> 
       </div>
-
-      <div class="three columns padding-top">
-        <a class="button button-primary" href="#">QUIENES SOMOS</a>
-      </div> 
     </div>
   </div>
 
